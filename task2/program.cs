9a50fda8d7e90e94f7ab9d480b741a7e43c1d121
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-20 18:04:35 +0300

    task2 completed

---
+++ task2/program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму 
+//цифр в числе.
+
+int prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int a = int.Parse(number);
+    return a;
+}
+
+int a = prompt("enter number: ");
+int SumA(int maxNumber)
+{
+    int answer = 0;
+    while (a > 0)
+    {
+        answer += a % 10;
+        a = a / 10;
+    }
+    return answer;
+}
+System.Console.WriteLine($"Sum {SumA(a)}");
